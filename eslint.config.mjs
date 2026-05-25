b206b8ab4a56140f919c1411feb7b74f64c7732f
Sapna Sapna <sapna.sapna.c@saucelabs.com>
2026-05-25 16:15:41 +0530

    Conflict handling 3

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+import ts from 'typescript-eslint';
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import cypress from 'eslint-plugin-cypress/flat';
+
+export default ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  prettier,
+  {
+    ignores: ['lib/**'],
+  },
+  {
+    files: ['**/*.*js', '**/*.*ts'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-undef': 'warn',
+    },
+  },
+  {
+    files: ['tests/**/*.*js', 'tests/**/*.*ts'],
+    ...cypress.configs.recommended,
+  },
+  {
+    languageOptions: {
+      globals: {
+        __dirname: true,
+        console: true,
+        exports: true,
+        module: true,
+        require: true,
+        process: true,
+        NodeJS: true,
+        CypressCommandLine: true,
+      },
+    },
+  },
+);
